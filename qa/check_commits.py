31f0364301fd1d02edc1600e11d5cbc9822dc4fe
jjvargass <jotavargas89@gmail.com>
2020-02-17 12:03:44 -0500

    refactor:ajustes en filtro de commits

--- qa/check_commits.py
+++ qa/check_commits.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
 import os
 from git import Repo
 import logging
@@ -7,46 +10,25 @@ import calendar
 date  = datetime.datetime.now()
 fecha_inicio = date.replace(day = 1)
 fecha_fin = date.replace(day = calendar.monthrange(date.year, date.month)[1])
+# formato
+form_fecha_inicio = fecha_inicio.strftime("%Y-%m-%d")
+form_fecha_fin = fecha_fin.strftime("%Y-%m-%d")
+# filtros
+# --after=2020-02-11 00:00
+after = '--after=' + str(form_fecha_inicio) +' 00:00'
+# --before=2020-02-12 23:59
+before = '--before=' + str(form_fecha_fin) + ' 23:59'
 
-print fecha_inicio
-print fecha_fin
+path = os.getcwd()
+repo = Repo(path)
 
-print "Formatos fecha Inicio"
-# dd/mm/YY
-d1 = fecha_inicio.strftime("%d/%m/%Y")
-print("d1 =", d1)
-# Textual month, day and year
-d2 = fecha_inicio.strftime("%B %d, %Y")
-print("d2 =", d2)
-# mm/dd/y
-d3 = fecha_inicio.strftime("%m/%d/%y")
-print("d3 =", d3)
-# Month abbreviation, day and year
-d4 = fecha_inicio.strftime("%b-%d-%Y")
-print("d4 =", d4)
+print "### Intervalo de {}  hasta {}  ###".format(form_fecha_inicio, form_fecha_fin)
 
-print "Formatos fecha Fin"
-# dd/mm/YY
-d1 = fecha_fin.strftime("%d/%m/%Y")
-print("d1 =", d1)
-# Textual month, day and year
-d2 = fecha_fin.strftime("%B %d, %Y")
-print("d2 =", d2)
-# mm/dd/y
-d3 = fecha_fin.strftime("%m/%d/%y")
-print("d3 =", d3)
-# Month abbreviation, day and year
-d4 = fecha_fin.strftime("%b-%d-%Y")
-print("d4 =", d4)
+commits_list = repo.git.log('--oneline', '--after=2020-02-11 00:00', '--before=2020-02-11 23:59', '--format=%B').split('\n')
 
+# limpiar lista
+while("" in commits_list):
+    commits_list.remove("")
 
-
-# path = os.getcwd()
-# repo = Repo(path)
-# commits_list = list(repo.iter_commits())
-
-# for i in range(len(commits_list)):
-#     commit = commits_list[i]
-#     print(commit.hexsha)
-#     print(commit.author)
-#     print(commit.message)
+for i in commits_list:
+    print i
