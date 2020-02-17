1b1e0c5426ddab466603a3a01c0f145524f4c68b
jjvargass <jotavargas89@gmail.com>
2020-02-17 12:11:04 -0500

    doc:ajustes fecha de un dia

--- qa/check_commits.py
+++ qa/check_commits.py
@@ -25,7 +25,7 @@ repo = Repo(path)
 print "### Intervalo de {}  hasta {}  ###".format(form_fecha_inicio, form_fecha_fin)
 
 #commits_list = repo.git.log('--oneline', after, before, '--format=%B').split('\n')
-commits_list = repo.git.log('--oneline', '--after=2020-02-11 00:00', '--before=2020-02-12 23:59', '--format=%B').split('\n')
+commits_list = repo.git.log('--oneline', '--after=2020-02-17 00:00', '--before=2020-02-17 23:59', '--format=%B').split('\n')
 
 # limpiar lista
 while("" in commits_list):
